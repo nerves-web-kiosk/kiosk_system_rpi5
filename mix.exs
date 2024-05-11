21a96a6391d15a1d0c59bc9351f7da25ffb92bef
Frank Hunleth <fhunleth@troodon-software.com>
2024-05-11 13:27:06 -0400

    Enable cog

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule NervesSystemRpi5.MixProject do
   use Mix.Project
 
   @github_organization "nerves-project"
-  @app :nerves_system_rpi5
+  @app :kiosk_system_rpi5
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
